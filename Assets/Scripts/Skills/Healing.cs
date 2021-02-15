8574342f9d54fe32e81b461a8d7d8137a1e637bb
michaelhans <michaelhans777@gmail.com>
2021-02-15 22:35:46 +0700

    Survival Shooter completed

---
+++ Assets/Scripts/Skills/Healing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Healing : PowerUp
+{
+    GameObject player;
+    PlayerHealth playerHealth;
+    public AudioSource healingAudio;
+
+    public override void Awake()
+    {
+        //Mencari game object dengan tag "Player"
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        //mendapatkan komponen player health
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+
+    //Callback jika ada suatu object masuk kedalam trigger
+    public override void OnTriggerEnter(Collider other)
+    {
+        //Set player in range
+        float dist = Vector3.Distance(other.transform.position, transform.position);
+        if ((other.tag == "Player") && (dist < 1.5))
+        {
+            playerHealth.Healing();
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Skills/SpeedUp.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class SpeedUp : PowerUp
+{
+    GameObject player;
+    PlayerMovement playerMovement;
+    public AudioSource speedUpAudio;
+
+    public override void Awake()
+    {
+        //Mencari game object dengan tag "Player"
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        //mendapatkan komponen player health
+        playerMovement = player.GetComponent<PlayerMovement>();
+    }
+
+    //Callback jika ada suatu object masuk kedalam trigger
+    public override void OnTriggerEnter(Collider other)
+    {
+        //Set player in range
+        float dist = Vector3.Distance(other.transform.position, transform.position);
+        if ((other.tag == "Player") && (dist < 1.5))
+        {
+            playerMovement.SpeedUp();
+            this.gameObject.SetActive(false);
+        }
+    }
+}
